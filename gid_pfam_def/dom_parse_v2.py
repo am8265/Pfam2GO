685307423fbca6196e704c2e206decded414e1f9
Ayan Malakar <mailme.ayan@gmail.com>
2016-09-24 18:08:05 -0400

    took out the junk code

--- gid_pfam_def/dom_parse_v2.py
+++ gid_pfam_def/dom_parse_v2.py
@@ -36,40 +36,7 @@ def gid_pfam(fh_1,eval=math.exp(-10)):
                 pass#we we just ignore the line if its above the particular evalue threshold 
     fh.close() 
     return gid_pfam_eval       
-#     for ids in sorted(gid_pfam_eval.keys()):
-         
 
-            
-#                if pfam_def.get(pfam_id,'0')=='0':
-#                    pfam_def[pfam_id]=[def_pfam]#every unique pfam_id shoudl have a unique definition
-#                else:
-#                    pfam_def[pfam_id].append(def_pfam)
-            
-#                if gid_pfam.get(gid,'0')=='0':
-#                    gid_pfam[gid]=[pfam_id]
-#                else:
-#                    gid_pfam[gid].append(pfam_id)
-    
-#print(gid_pfam)
-#    for ids in sorted(gid_pfam.keys()):
-#        gid_pfam[ids]=list(set(gid_pfam[ids]))
-#    for ids in pfam_def.keys():
-#        pfam_def[ids]=list(set(pfam_def[ids]))
-#    return (gid_pfam,pfam_def)
-#    fh.close()
-
-gid_pfam_eval=gid_pfam(sys.argv[1])
-print(gid_pfam_eval)
- 
-
-
-
-
-#id_pfam=gid_pfam[0]
-#pfam_def=gid_pfam[1]
-######testing here
-#print(id_pfam)
-##########
 out_fh=open('gid_pfam_def.txt','w')
 out_fh.write('peptide_id'+'\t'+'pfam_id'+'\t'+'definition'+'\t'+'Evalues'+'\n')
 for ids in sorted(gid_pfam_eval.keys()):
@@ -86,15 +53,7 @@ for ids in sorted(gid_pfam_eval.keys()):
     pfam_id_cat=pfam_id_cat.strip().rstrip(',')
     pfam_def_cat=pfam_def_cat.strip().rstrip(';')
     eval_min_cat=eval_min_cat.strip().rstrip(';')
-    out_fh.write(ids+'\t'+pfam_id_cat+'\t'+pfam_def_cat+'\t'+eval_min_cat+'\n')    
- #   pfm_id=str(id_pfam[ids]).strip(']').strip('[')#you can split and join in future?
- #   pf_def=''#pfa_def means pfam[def]
- #   for p_ids in id_pfam[ids]:
- #       for defi in pfam_def[p_ids]:
- #           pf_def+=str(p_ids)+str([defi])+'; '
- #   pf_def=pf_def.strip().rstrip(';')
- #   pfm_id=pfm_id.strip(',')
- #   out_fh.write(str(ids)+'\t'+pfm_id+'\t'+pf_def+'\n')
-#for ids in gid_pfam.keys():
-#    print(ids)
+    out_fh.write(ids+'\t'+pfam_id_cat+'\t'+pfam_def_cat+'\t'+eval_min_cat+'\n')
+gid_pfam_eval=gid_pfam(sys.argv[1])
+print(gid_pfam_eval)
 
